e0a2a33e053ccab66806a177b2726d76e3480524
Sanjay Kandel <kandelsanjay941@gmail.com>
2022-03-19 23:42:07 +0530

    contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,7 +16,23 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+const privateKey =
+  "410990e9b0da30cef16239f9a235b0017728be4ee3c7b3c8d84abe9fbe0b7245";
+
 module.exports = {
   solidity: "0.8.4",
- 
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/51c85accff9e4b9297e6fcc96bbae274`,
+      accounts: [privateKey],
+    },
+    alchemy: {
+      url: "https://polygon-mumbai.g.alchemy.com/v2/Ao4G6y8WzXsmV-QYoXBcCwRLQchYjEef",
+      accounts: [privateKey],
+    },
+  },
 };
