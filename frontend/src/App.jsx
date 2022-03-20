16e355a293aefdb46dcf0bd7e45c903896c68a3b
Sanjay Kandel <kandelsanjay941@gmail.com>
2022-03-20 16:37:43 +0530

    wallet interaction

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,67 +1,71 @@
-import { useState } from "react";
+import React,{ useStatem,useEffect } from "react";
+import { useMoralis } from "react-moralis";
+
+import SideBar from './components/SideBar'
+
+
 
 const App = () => {
-  const [open, setOpen] = useState(true);
-  const Menus = [
-    { title: "Dashboard", src: "Chart_fill" },
-    { title: "Inbox", src: "Chat" },
-    { title: "Accounts", src: "User", gap: true },
-    { title: "Schedule ", src: "Calendar" },
-    { title: "Search", src: "Search" },
-    { title: "Analytics", src: "Chart" },
-    { title: "Files ", src: "Folder", gap: true },
-    { title: "Setting", src: "Setting" },
-  ];
+  const {
+    authenticate,
+    isWeb3Enabled,
+    isAuthenticated,
+    user,
+    enableWeb3,
+    Moralis,
+  } = useMoralis();
 
+  async function authWalletConnect() {
+    const user = await authenticate({
+      provider: "walletconnect",
+      chainId: 80001,
+      signingMessage: "welcome"
+    });
+    
+    console.log(user);
+  }
+
+  useEffect(()=>{
+    if (!isWeb3Enabled && isAuthenticated){
+      enableWeb3({provider: "walletconnect",chainId:80001});
+      console.log("web3 activated");
+    }
+  },[isWeb3Enabled,isAuthenticated,enableWeb3]);
+
+  document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState === "hidden") {
+      window.localStorage.removeItem("WALLETCONNECT_DEEPLINK_CHOICE");
+    }
+  });
+
+  if(!isAuthenticated && !user){
+    console.log(user);
+    return (
+      <div>
+        <h1> Wallet Authentication </h1>
+        <div>
+          <button className="bg-green-500 rounded-xl p-2 mb-3" onClick={()=>authenticate({signingMessage: "Welcome"})}>
+            Signin Using Metamask
+          </button>
+        </div>
+        <div>
+          {/*<button className="bg-teal-500 rounded-xl p-2" onClick={()=>authWalletConnect()}>
+            Signin Using Wallet Connect
+          </button>*/}
+        </div>
+      </div>
+    )
+  }
   return (
     <div className="flex">
-      <div
-        className={` ${
-          open ? "w-72" : "w-20 "
-        } bg-dark-purple h-screen p-5  pt-8 relative duration-300`}
-      >
-        <img
-          src="./src/assets/control.png"
-          className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
-           border-2 rounded-full  ${!open && "rotate-180"}`}
-          onClick={() => setOpen(!open)}
-        />
-        <div className="flex gap-x-4 items-center">
-          <img
-            src="./src/assets/logo.png"
-            className={`cursor-pointer duration-500 ${
-              open && "rotate-[360deg]"
-            }`}
-          />
-          <h1
-            className={`text-white origin-left font-medium text-xl duration-200 ${
-              !open && "scale-0"
-            }`}
-          >
-            Designer
-          </h1>
-        </div>
-        <ul className="pt-6">
-          {Menus.map((Menu, index) => (
-            <li
-              key={index}
-              className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 
-              ${Menu.gap ? "mt-9" : "mt-2"} ${
-                index === 0 && "bg-light-white"
-              } `}
-            >
-              <img src={`./src/assets/${Menu.src}.png`} />
-              <span className={`${!open && "hidden"} origin-left duration-200`}>
-                {Menu.title}
-              </span>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <SideBar/>
       <div className="h-screen flex-1 p-7">
+        {/*<LogoutButton/>   */}     
         <h1 className="text-2xl font-semibold ">Home Page</h1>
       </div>
     </div>
   );
 };
+
+
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,23 @@
-import reactDom from "react-dom";
+//external imports
+import React from "react";
+import ReactDOM from "react-dom";
+import {MoralisProvider} from "react-moralis";
+
+//internal imports
 import App from "./App";
 import "./index.css";
 
-reactDom.render(<App />, document.getElementById("root"));
+// IMP : Dont use hardcoded values
+const moralisAppId = "TB3hFSbs20LtUCQgdiolte1O9GujtcpDTzR5mVUx";
+const moralisServerURL = "https://hj2l8fsvhhqj.usemoralis.com:2053/server";
+
+// reactDom.render(<App />, document.getElementById("root"));
+
+ReactDOM.render(
+	<React.StrictMode>
+		<MoralisProvider appId={moralisAppId} serverUrl={moralisServerURL}>
+			<App />
+		</MoralisProvider>
+	</React.StrictMode>,
+	document.getElementById("root")
+);
