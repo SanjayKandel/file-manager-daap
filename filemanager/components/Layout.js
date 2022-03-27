7e36a208139e0cb1046858601b2e8aa49b0a9899
Sanjay Kandel <kandelsanjay941@gmail.com>
2022-03-27 11:08:37 +0530

    .

--- filemanager/components/Layout.js
+++ filemanager/components/Layout.js
@@ -1,13 +1,6 @@
 import SideBar from "./SideBar";
-import { useMoralis } from "react-moralis";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 const Layout = ({ children }) => {
-  if (children.type.name === "Home") {
-    return <div>{children}</div>;
-  }
-
   return (
     <div className="flex">
       <SideBar />
--- filemanager/pages/upload.js
+++ filemanager/pages/upload.js
@@ -1,23 +1,26 @@
 import React, { useState, useEffect } from "react";
 import UploadFile from "../components/UploadFile";
 import { useMoralis } from "react-moralis";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 
+import SideBar from "../components/SideBar";
 
-export default function Upload(){
-	const router = useRouter();
-    const {isAuthenticated} = useMoralis();
-    async function getInitialProps(){
-	    if(!isAuthenticated){
-	        router.push('/')
-	    }
+export default function Upload() {
+  const router = useRouter();
+  const { isAuthenticated, logout } = useMoralis();
+  async function getInitialProps() {
+    if (!isAuthenticated) {
+      router.push("/");
     }
-	return (
-		<div className="h-screen flex-1 p-7">
-		      {/*<LogoutButton/>   */}
-		      <h1 className="text-2xl font-semibold text-">Upload File</h1>		      
-		      <UploadFile />
-	    </div>
-	)
-
+  }
+  return (
+    <div className="flex">
+      <SideBar />
+      <div className="h-screen flex-1 p-7">
+        {/*<LogoutButton/>   */}
+        <h1 className="text-2xl font-semibold text-">Upload File</h1>
+        <UploadFile />
+      </div>
+    </div>
+  );
 }
