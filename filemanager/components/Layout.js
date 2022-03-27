06616d56483b512262ed1b09553e7e917c55bf98
Sanjay Kandel <kandelsanjay941@gmail.com>
2022-03-27 08:43:48 +0530

    .

--- filemanager/components/Layout.js
+++ filemanager/components/Layout.js
@@ -1,22 +1,19 @@
-import SideBar from './SideBar'
+import SideBar from "./SideBar";
 import { useMoralis } from "react-moralis";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Layout = ({ children }) => {
-    
-    if(children[0].type.name==="Home"){
-        return (
-            <div>
-                {children}
-            </div>
-        )
-    }
-    return (
-        <div className="flex">
-          <SideBar />
-          {children}
-        </div>
-    )
-}
+  if (children.type.name === "Home") {
+    return <div>{children}</div>;
+  }
+
+  return (
+    <div className="flex">
+      <SideBar />
+      {children}
+    </div>
+  );
+};
 
 export default Layout;
